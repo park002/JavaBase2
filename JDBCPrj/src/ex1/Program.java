9306b27b7905615393facf05dc665c95c97af9f9
pjh <ekem159@naver.com>
2020-01-27 20:48:35 +0900

    뉴렉처 JDBC 7/7

--- JDBCPrj/src/ex1/Program.java
+++ JDBCPrj/src/ex1/Program.java
@@ -7,7 +7,7 @@ public class Program {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		String url = "jdbc:mysql://localhost/new";
-		String sql = "select * from notice";
+		String sql = "select * from notice where hit>=10";
 
 		Class.forName("com.mysql.jdbc.Driver"); // 드라이버 로드
 		// @param getConnection(url, userName, password);
@@ -22,13 +22,10 @@ public class Program {
 			Date regdate = rs.getDate("regdate");
 			int hit = rs.getInt("hit");
 			String Files = rs.getString("Files");
-			System.out.println(title);
-			System.out.println(id);
-			System.out.println(writer_id);
-			System.out.println(content);
-			System.out.println(regdate);
-			System.out.println(hit);
-			System.out.println(Files);
+			System.out.printf(
+					"ID=>%d입니다. title=>%s 입니다. writer_id=>%s입니다. content=> %s 입니다. regdate=>%s입니다 . hit=>%d 입니다. Files=>%s 입니다.",
+					id, title, writer_id, content, regdate, hit, Files);
+			System.out.println("===========");
 		}
 		rs.close();
 		st.close();
