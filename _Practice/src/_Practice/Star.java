2cd849c441662ce05a1526757addb5e41fb44bbc
pjh <ekem159@naver.com>
2020-04-23 16:47:28 +0900

    효율적인 연산 공부

--- _Practice/src/_Practice/Star.java
+++ _Practice/src/_Practice/Star.java
@@ -7,16 +7,20 @@ public class Star {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		byte p = 10;
-		byte n = -10;
-		System.out.printf("p = %d \t%s%n", p,toBinaryString(p));
-		System.out.println("\t\t00000000000000000000000000001010");
-	}
-
-	static String toBinaryString(int x) { // 10진수를 2진수로 변환하는 메소드
-		String zero = "00000000000000000000000000000000";
-		String tmp = zero + Integer.toBinaryString(x); //2진수로 변환
-		return tmp.substring(tmp.length() - 32);
+		Scanner sc = new Scanner(System.in);
+		char ch=' ';
+		System.out.printf("문자를 하나 입력하세요>");
+		String input =sc.nextLine();
+		ch = input.charAt(0);
+		if('0'<=ch && ch<='9') {
+			System.out.printf("입력하신 문자는 숫자입니다.%n");
+		}
+		else if (('a'<=ch && ch<='z') || ('A'<=ch && ch<='Z')) {
+			System.out.println("입력하신 문자는 문자입니다.");
+			
+		}
+		
+		
 
 	}
    
