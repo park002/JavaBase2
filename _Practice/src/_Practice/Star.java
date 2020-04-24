8a36cb56c872b2975c245aa403de375db3663e2c
pjh <ekem159@naver.com>
2020-04-25 01:09:12 +0900

    구구단,별찍기

--- _Practice/src/_Practice/Star.java
+++ _Practice/src/_Practice/Star.java
@@ -6,29 +6,17 @@ public class Star {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			 System.out.println("당신의 주민번호를 입력하세요.(011231-1111222)>");
-			 Scanner sc  = new Scanner(System.in);
-			 String Jumin = 	sc.nextLine();
-			 char Sung = Jumin.charAt(7);
-			 switch(Sung) {
-			 	case '1':
-				case '3':
-					switch(Sung) {
-						case '1': System.out.println("당신은 2000년 이전에 태어난 남자입니다");
-						break;
-						case '3': System.out.println("당신은 2000년 이후에 태어난 남자입니다");
+			System.out.println("몇 단을 출력해볼까요? ~>");
+			Scanner sc = new Scanner(System.in);
+			String abc =  sc.nextLine();
+			int gugu =  Integer.parseInt(abc);
+			for(int i=gugu; i<=gugu; i++) {
+					for(int j=1; j<=9; j++) {
+			
+						System.out.printf("%d x %d = %d",i,j,i*j);
+						System.out.println();
 					}
-				 break;
-				case '2':
-				case '4': 
-				 	switch(Sung) {
-				 	case '2' : System.out.println("당신은 2000년 이전에 태어난 여자입니다");
-				 	break;
-				 	case '4' : System.out.println("당신은 2000년 이후에 태어난 여자 입니다");
-				 }
-				 	break;
-				 
-				 default: System.out.println("유효하지 않는 주민등록번호입니다");
-			 }
+			}
+			
 	}
 }
