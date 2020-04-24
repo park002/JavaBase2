0aae4fd787a12710e570e7ea6537eecf425f2b8e
pjh <ekem159@naver.com>
2020-04-24 21:22:11 +0900

    Switch 의 중복

--- _Practice/src/_Practice/Star.java
+++ _Practice/src/_Practice/Star.java
@@ -1,28 +1,34 @@
 package _Practice;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Star {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		char ch=' ';
-		System.out.printf("문자를 하나 입력하세요>");
-		String input =sc.nextLine();
-		ch = input.charAt(0);
-		if('0'<=ch && ch<='9') {
-			System.out.printf("입력하신 문자는 숫자입니다.%n");
-		}
-		else if (('a'<=ch && ch<='z') || ('A'<=ch && ch<='Z')) {
-			System.out.println("입력하신 문자는 문자입니다.");
-			
-		}
-		
-		
-
+			 System.out.println("당신의 주민번호를 입력하세요.(011231-1111222)>");
+			 Scanner sc  = new Scanner(System.in);
+			 String Jumin = 	sc.nextLine();
+			 char Sung = Jumin.charAt(7);
+			 switch(Sung) {
+			 	case '1':
+				case '3':
+					switch(Sung) {
+						case '1': System.out.println("당신은 2000년 이전에 태어난 남자입니다");
+						break;
+						case '3': System.out.println("당신은 2000년 이후에 태어난 남자입니다");
+					}
+				 break;
+				case '2':
+				case '4': 
+				 	switch(Sung) {
+				 	case '2' : System.out.println("당신은 2000년 이전에 태어난 여자입니다");
+				 	break;
+				 	case '4' : System.out.println("당신은 2000년 이후에 태어난 여자 입니다");
+				 }
+				 	break;
+				 
+				 default: System.out.println("유효하지 않는 주민등록번호입니다");
+			 }
 	}
-   
-
 }
