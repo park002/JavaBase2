dfdbbacfd8c763d61bb4206dc5641ed0bc8abfba
pjh <ekem159@naver.com>
2020-04-28 22:10:14 +0900

    배열의 복사

--- _Practice/src/_Practice/Star.java
+++ _Practice/src/_Practice/Star.java
@@ -1,53 +1,26 @@
 package _Practice;
 
-import java.util.Scanner;
-
 public class Star {
 
 	public static void main(String[] args) {
-		int menu = 0, num = 0;
-		Scanner scanner = new Scanner(System.in);
-
-		outer: while (true) {
-			System.out.println("(1) square");
-			System.out.println("(2) square root");
-			System.out.println("(3) log");
-			System.out.println("원하는 메뉴를 선택하세요 (1~3) 종료:(0) >");
-
-			String tmp = scanner.nextLine();
-			menu = Integer.parseInt(tmp);
-			
-			if (menu == 0) {
-				System.out.println("프로그램을 종료합니다");
-				break;
-			} else if (!(1 <= menu && menu <= 3)) {
-				System.out.println("메뉴를 잘 못선택하셨습니다 종료는 0 입니다");
-				continue;
-			}
-
-			for (;;) { // 선택 잘했으면 여기로온다
-				System.out.print("계산 할 값을 입력하세요.(계산종료:0,전체 종료:99)>");
-				tmp = scanner.nextLine(); // 화면에서 입력받은 내용을 tmp에 저장
-				num = Integer.parseInt(tmp);
-				if (num == 0)
-					break;
-				if (num == 99)
-					break outer; // 전체종료
-				switch (menu) {
-				case 1:
-					System.out.println("result=>" + num * num);
-					break;
-				case 2:
-					System.out.println("result=>" + Math.sqrt(num));
-					break;
-				case 3:
-					System.out.println("result=>" + Math.log(num));
-					break;
-				}
-
-			}
-
+		int[] arr = new int[5];
+		System.out.println("[변경전]");
+		System.out.println("arr.length:" + arr.length);
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i+1;
+			System.out.println("arr[" + i + "]" + arr[i]);
 		}
-
+		System.out.println("변경 후");
+		int[] temp = new int[arr.length * 2];
+		
+		for(int i=0; i<arr.length; i++)
+			temp[i] = arr[i];
+			arr=temp; //arr참조변수 는 temp를 참조한다 
+			//배열은 참조변수를 통해서만 접근 할 수 있고 , 자신을 가리키는 참조변수가 없는 배열은 사용 할 수 없다 .
+		System.out.println(arr.length);
+		for(int i=0; i<arr.length; i++) 
+			System.out.println("arr["+i+"]"+arr[i]);
+		
 	}
 }
