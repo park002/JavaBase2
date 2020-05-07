e442d7aa9f80015f669c7487e2f6c20375d7ebbb
pjh <ekem159@naver.com>
2020-05-07 18:50:18 +0900

    char 2byte 0을포함  크기 65535 까지

--- _Practice/src/_Practice/Star.java
+++ _Practice/src/_Practice/Star.java
@@ -1,24 +1,16 @@
 package _Practice;
 
-
-
 public class Star {
 	public static void main(String[] args) {
-		System.out.println("변수란 무엇인가");
-		System.out.println("변수란 값을 담을 공간");
-		int year = 0; //변수의 초기화
-		int age = 20;
-		System.out.println(year);
-		System.out.println(age);
-		year = age + 2000;
-		age = age+5;
-		System.out.println(year);
-		System.out.println(age);
-		System.out.println("연습");
+		byte b = 10;
+		char ch = 'A';
+		int i = 100;
+		long l = 1000L;
+		int If = 3;
 		
+		char bb = 65535;
+		System.out.println(bb);
 		
-		
-		
-		
-}
+		System.out.println(ch);
+	}
 }
