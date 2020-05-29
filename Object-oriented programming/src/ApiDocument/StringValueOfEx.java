f221f337cc9d38b79cb8572a9e64c8385eb373ce
pjh <ekem159@naver.com>
2020-05-30 00:13:08 +0900

    java.lang 패키지 (2/3) && String 클래스의 주요 메소드

---
+++ Object-oriented programming/src/ApiDocument/StringValueOfEx.java
@@ -0,0 +1,13 @@
+package ApiDocument;
+
+public class StringValueOfEx {
+	public static void main(String[] args) {
+		String str1 = String.valueOf(10);
+		String str2 = String.valueOf(20.2);
+		String str3 = String.valueOf(true);
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		
+	}
+}
